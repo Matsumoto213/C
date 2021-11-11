eff3e014acfe0812c01fe2c11fd95a6f50a31dcf
yusuke <yusuke.213@icloud.com>
2021-11-11 16:08:25 +0900

    11月11日の更新

---
+++ practice7.9_7-1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <time.h>
+
+typedef char String[1024];
+
+
+int main(void){
+    int scores[5] = {};
+    char primary[3] = {'R','G','B'};
+    int averages[30];
+    int table[9][9];
+    return 0;
+}
