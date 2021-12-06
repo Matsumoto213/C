d8b6fce141ae230cbd737d226ae8c69a9ab48b34
yusuke <yusuke.213@icloud.com>
2021-12-06 20:55:55 +0900

    関数の作成

---
+++ 関数2.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <time.h>
+
+typedef char String[1024];
+
+int iscanf(void){
+    String inputstr;
+    scanf("%s",inputstr);
+    return atoi(inputstr);
+}
+
+int main(void){
+    printf("");
+    return 0;
+}
